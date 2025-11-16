6734a0f430f0a25e0cd72f500770fbac8fdfc1cc
Luís Pires <luisfrpires18@gmail.com>
2025-11-16 00:23:18 +0000

    fix ranking

--- src/RTUB.Application/Configuration/RankingConfiguration.cs
+++ src/RTUB.Application/Configuration/RankingConfiguration.cs
@@ -7,8 +7,6 @@ namespace RTUB.Application.Configuration;
 public class RankingConfiguration
 {
     public const string SectionName = "Ranking";
-    
-    public bool Enabled { get; set; } = true;
     public int XpPerRehearsal { get; set; } = 10;
     public Dictionary<string, int> XpPerEventType { get; set; } = new();
     public List<LevelDefinition> Levels { get; set; } = new();
