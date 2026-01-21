02efbb43819e94f09b126a83d8049605357b1c84
Luís Pires <luisfrpires18@gmail.com>
2026-01-21 17:40:33 +0000

    cache

--- src/RTUB.Web/wwwroot/service-worker.js
+++ src/RTUB.Web/wwwroot/service-worker.js
@@ -2,7 +2,7 @@
 // Handles push events, notification clicks, and offline asset caching
 
 // Cache version - increment when updating service worker
-const CACHE_VERSION = 'rtub-v14';
+const CACHE_VERSION = 'rtub-v15';
 const STATIC_CACHE = `rtub-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `rtub-dynamic-${CACHE_VERSION}`;
 const IMAGE_CACHE = `rtub-images-${CACHE_VERSION}`;
