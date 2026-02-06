55669edc6d88c5996121d2548a21ca16ef1741a6
Luís Pires <luisfrpires18@gmail.com>
2026-02-06 02:40:05 +0000

    cache

--- src/RTUB.Web/wwwroot/service-worker.js
+++ src/RTUB.Web/wwwroot/service-worker.js
@@ -6,7 +6,7 @@
 // Optimized for mobile PWA performance
 
 // Cache version - increment when updating service worker
-const CACHE_VERSION = 'rtub-v26';
+const CACHE_VERSION = 'rtub-v27';
 const STATIC_CACHE = `rtub-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `rtub-dynamic-${CACHE_VERSION}`;
 const IMAGE_CACHE = `rtub-images-${CACHE_VERSION}`;
